8a1b69ea41ba2102f4cb853a4283abe618983666
Conor Sayres <csayres@uw.edu>
2014-05-30 10:24:05 -0700

    niced

--- rectFlows.sh
+++ rectFlows.sh
@@ -1,124 +1,122 @@
 #!/bin/bash
-./a.out 100 1 10 &
-./a.out 100 1 30 &
-./a.out 100 1 50 &
-./a.out 100 1 70 &
-./a.out 100 1 100 &
-./a.out 100 1 200 &
-./a.out 100 1 300 &
-./a.out 100 1 500 &
-./a.out 100 1 1000 &
-./a.out 100 1 1500 &
-./a.out 100 1 2000 &
+nice -n7 ./a.out 100 1 10 &
+nice -n7 ./a.out 100 1 30 &
+nice -n7 ./a.out 100 1 50 &
+nice -n7 ./a.out 100 1 70 &
+nice -n7 ./a.out 100 1 100 &
+nice -n7 ./a.out 100 1 200 &
+nice -n7 ./a.out 100 1 300 &
+nice -n7 ./a.out 100 1 500 &
+nice -n7 ./a.out 100 1 1000 &
+nice -n7 ./a.out 100 1 1500 &
+nice -n7 ./a.out 100 1 2000 &
 
-./a.out 100 2 10 &
-./a.out 100 2 30 &
-./a.out 100 2 50 &
-./a.out 100 2 70 &
-./a.out 100 2 100 &
-./a.out 100 2 200 &
-./a.out 100 2 300 &
-./a.out 100 2 500 &
-./a.out 100 2 1000 &
-./a.out 100 2 1500 &
-./a.out 100 2 2000 &
+nice -n7 ./a.out 100 2 10 &
+nice -n7 ./a.out 100 2 30 &
+nice -n7 ./a.out 100 2 50 &
+nice -n7 ./a.out 100 2 70 &
+nice -n7 ./a.out 100 2 100 &
+nice -n7 ./a.out 100 2 200 &
+nice -n7 ./a.out 100 2 300 &
+nice -n7 ./a.out 100 2 500 &
+nice -n7 ./a.out 100 2 1000 &
+nice -n7 ./a.out 100 2 1500 &
+nice -n7 ./a.out 100 2 2000 &
 
-./a.out 100 3 10 &
-./a.out 100 3 30 &
-./a.out 100 3 50 &
-./a.out 100 3 70 &
-./a.out 100 3 100 &
-./a.out 100 3 200 &
-./a.out 100 3 300 &
-./a.out 100 3 500 &
-./a.out 100 3 1000 &
-./a.out 100 3 1500 &
-./a.out 100 3 2000 &
+nice -n7 ./a.out 100 3 10 &
+nice -n7 ./a.out 100 3 30 &
+nice -n7 ./a.out 100 3 50 &
+nice -n7 ./a.out 100 3 70 &
+nice -n7 ./a.out 100 3 100 &
+nice -n7 ./a.out 100 3 200 &
+nice -n7 ./a.out 100 3 300 &
+nice -n7 ./a.out 100 3 500 &
+nice -n7 ./a.out 100 3 1000 &
+nice -n7 ./a.out 100 3 1500 &
+nice -n7 ./a.out 100 3 2000 &
 
+nice -n7 ./a.out 100 4 10 &
+nice -n7 ./a.out 100 4 30 &
+nice -n7 ./a.out 100 4 50 &
+nice -n7 ./a.out 100 4 70 &
+nice -n7 ./a.out 100 4 100 &
+nice -n7 ./a.out 100 4 200 &
+nice -n7 ./a.out 100 4 300 &
+nice -n7 ./a.out 100 4 500 &
+nice -n7 ./a.out 100 4 1000 &
+nice -n7 ./a.out 100 4 1500 &
+nice -n7 ./a.out 100 4 2000 &
 
-./a.out 100 4 10 &
-./a.out 100 4 30 &
-./a.out 100 4 50 &
-./a.out 100 4 70 &
-./a.out 100 4 100 &
-./a.out 100 4 200 &
-./a.out 100 4 300 &
-./a.out 100 4 500 &
-./a.out 100 4 1000 &
-./a.out 100 4 1500 &
-./a.out 100 4 2000 &
+nice -n7 ./a.out 100 5 10 &
+nice -n7 ./a.out 100 5 30 &
+nice -n7 ./a.out 100 5 50 &
+nice -n7 ./a.out 100 5 70 &
+nice -n7 ./a.out 100 5 100 &
+nice -n7 ./a.out 100 5 200 &
+nice -n7 ./a.out 100 5 300 &
+nice -n7 ./a.out 100 5 500 &
+nice -n7 ./a.out 100 5 1000 &
+nice -n7 ./a.out 100 5 1500 &
+nice -n7 ./a.out 100 5 2000 &
 
+nice -n7 ./a.out 100 6 10 &
+nice -n7 ./a.out 100 6 30 &
+nice -n7 ./a.out 100 6 50 &
+nice -n7 ./a.out 100 6 70 &
+nice -n7 ./a.out 100 6 100 &
+nice -n7 ./a.out 100 6 200 &
+nice -n7 ./a.out 100 6 300 &
+nice -n7 ./a.out 100 6 500 &
+nice -n7 ./a.out 100 6 1000 &
+nice -n7 ./a.out 100 6 1500 &
+nice -n7 ./a.out 100 6 2000 &
 
-./a.out 100 5 10 &
-./a.out 100 5 30 &
-./a.out 100 5 50 &
-./a.out 100 5 70 &
-./a.out 100 5 100 &
-./a.out 100 5 200 &
-./a.out 100 5 300 &
-./a.out 100 5 500 &
-./a.out 100 5 1000 &
-./a.out 100 5 1500 &
-./a.out 100 5 2000 &
+nice -n7 ./a.out 100 7 10 &
+nice -n7 ./a.out 100 7 30 &
+nice -n7 ./a.out 100 7 50 &
+nice -n7 ./a.out 100 7 70 &
+nice -n7 ./a.out 100 7 100 &
+nice -n7 ./a.out 100 7 200 &
+nice -n7 ./a.out 100 7 300 &
+nice -n7 ./a.out 100 7 500 &
+nice -n7 ./a.out 100 7 1000 &
+nice -n7 ./a.out 100 7 1500 &
+nice -n7 ./a.out 100 7 2000 &
 
-./a.out 100 6 10 &
-./a.out 100 6 30 &
-./a.out 100 6 50 &
-./a.out 100 6 70 &
-./a.out 100 6 100 &
-./a.out 100 6 200 &
-./a.out 100 6 300 &
-./a.out 100 6 500 &
-./a.out 100 6 1000 &
-./a.out 100 6 1500 &
-./a.out 100 6 2000 &
+nice -n7 ./a.out 100 8 10 &
+nice -n7 ./a.out 100 8 30 &
+nice -n7 ./a.out 100 8 50 &
+nice -n7 ./a.out 100 8 70 &
+nice -n7 ./a.out 100 8 100 &
+nice -n7 ./a.out 100 8 200 &
+nice -n7 ./a.out 100 8 300 &
+nice -n7 ./a.out 100 8 500 &
+nice -n7 ./a.out 100 8 1000 &
+nice -n7 ./a.out 100 8 1500 &
+nice -n7 ./a.out 100 8 2000 &
 
-./a.out 100 7 10 &
-./a.out 100 7 30 &
-./a.out 100 7 50 &
-./a.out 100 7 70 &
-./a.out 100 7 100 &
-./a.out 100 7 200 &
-./a.out 100 7 300 &
-./a.out 100 7 500 &
-./a.out 100 7 1000 &
-./a.out 100 7 1500 &
-./a.out 100 7 2000 &
+nice -n7 ./a.out 100 9 10 &
+nice -n7 ./a.out 100 9 30 &
+nice -n7 ./a.out 100 9 50 &
+nice -n7 ./a.out 100 9 70 &
+nice -n7 ./a.out 100 9 100 &
+nice -n7 ./a.out 100 9 200 &
+nice -n7 ./a.out 100 9 300 &
+nice -n7 ./a.out 100 9 500 &
+nice -n7 ./a.out 100 9 1000 &
+nice -n7 ./a.out 100 9 1500 &
+nice -n7 ./a.out 100 9 2000 &
 
-./a.out 100 8 10 &
-./a.out 100 8 30 &
-./a.out 100 8 50 &
-./a.out 100 8 70 &
-./a.out 100 8 100 &
-./a.out 100 8 200 &
-./a.out 100 8 300 &
-./a.out 100 8 500 &
-./a.out 100 8 1000 &
-./a.out 100 8 1500 &
-./a.out 100 8 2000 &
-
-./a.out 100 9 10 &
-./a.out 100 9 30 &
-./a.out 100 9 50 &
-./a.out 100 9 70 &
-./a.out 100 9 100 &
-./a.out 100 9 200 &
-./a.out 100 9 300 &
-./a.out 100 9 500 &
-./a.out 100 9 1000 &
-./a.out 100 9 1500 &
-./a.out 100 9 2000 &
-
-./a.out 100 10 10 &
-./a.out 100 10 30 &
-./a.out 100 10 50 &
-./a.out 100 10 70 &
-./a.out 100 10 100 &
-./a.out 100 10 200 &
-./a.out 100 10 300 &
-./a.out 100 10 500 &
-./a.out 100 10 1000 &
-./a.out 100 10 1500 &
-./a.out 100 10 2000 &
+nice -n7 ./a.out 100 10 10 &
+nice -n7 ./a.out 100 10 30 &
+nice -n7 ./a.out 100 10 50 &
+nice -n7 ./a.out 100 10 70 &
+nice -n7 ./a.out 100 10 100 &
+nice -n7 ./a.out 100 10 200 &
+nice -n7 ./a.out 100 10 300 &
+nice -n7 ./a.out 100 10 500 &
+nice -n7 ./a.out 100 10 1000 &
+nice -n7 ./a.out 100 10 1500 &
+nice -n7 ./a.out 100 10 2000 &
 
 
