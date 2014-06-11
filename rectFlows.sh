f2a5c4c664c489ca111ee6452cec8355cd194a09
Conor Sayres <csayres@uw.edu>
2014-06-11 08:55:29 -0700

    updated shell script

--- rectFlows.sh
+++ rectFlows.sh
@@ -1,62 +1,19 @@
 #!/bin/bash
 
-nice -n7 ./a.out 100 1 125 &
+nice -n7 ./a.out 50 1 50 &
+nice -n7 ./a.out 50 1 70 &
+nice -ny ./a.out 50 1 100 &
+nice -n7 ./a.out 50 1 150 &
+nice -n7 ./a.out 50 1 200 &
+
+nice -n7 ./a.out 100 1 50 &
+nice -n7 ./a.out 100 1 70 &
+nice -ny ./a.out 100 1 100 &
 nice -n7 ./a.out 100 1 150 &
-nice -n7 ./a.out 100 1 175 &
+nice -n7 ./a.out 100 1 200 &
 
-
-
-nice -n7 ./a.out 100 2 125 &
-nice -n7 ./a.out 100 2 150 &
-nice -n7 ./a.out 100 2 175 &
-
-
-
-nice -n7 ./a.out 100 3 125 &
-nice -n7 ./a.out 100 3 150 &
-nice -n7 ./a.out 100 3 175 &
-
-
-
-nice -n7 ./a.out 100 4 125 &
-nice -n7 ./a.out 100 4 150 &
-nice -n7 ./a.out 100 4 175 &
-
-
-
-nice -n7 ./a.out 100 5 125 &
-nice -n7 ./a.out 100 5 150 &
-nice -n7 ./a.out 100 5 175 &
-
-
-
-nice -n7 ./a.out 100 6 125 &
-nice -n7 ./a.out 100 6 150 &
-nice -n7 ./a.out 100 6 175 &
-
-
-
-nice -n7 ./a.out 100 7 125 &
-nice -n7 ./a.out 100 7 150 &
-nice -n7 ./a.out 100 7 175 &
-
-
-
-nice -n7 ./a.out 100 8 125 &
-nice -n7 ./a.out 100 8 150 &
-nice -n7 ./a.out 100 8 175 &
-
-
-
-nice -n7 ./a.out 100 9 125 &
-nice -n7 ./a.out 100 9 150 &
-nice -n7 ./a.out 100 9 175 &
-
-
-
-nice -n7 ./a.out 100 10 125 &
-nice -n7 ./a.out 100 10 150 &
-nice -n7 ./a.out 100 10 175 &
-
-
-
+nice -n7 ./a.out 200 1 50 &
+nice -n7 ./a.out 200 1 70 &
+nice -ny ./a.out 200 1 100 &
+nice -n7 ./a.out 200 1 150 &
+nice -n7 ./a.out 200 1 200 &
